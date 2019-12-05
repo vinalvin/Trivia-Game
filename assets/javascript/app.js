91d8ad04c7ea0466460f43c3560e3d4cd5bcb339
Junming Zhang <vinalvin1010@gmail.com>
2019-12-05 04:12:28 -0800

    question lists added

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+//Question lists
+var questions = [{
+        question: "1. What is the capital of Canada?",
+        choices: [
+            "A. Bangkok",
+            "B. Beijing",
+            "C. Ottawa",
+            "D. Stockholm",
+        ],
+        answer: 2,
+    },
+    {
+        question: "2. What is the capital of Indonesia?",
+        choices: [
+            "A. Bangkok",
+            "B. Beijing",
+            "C. Jakarta",
+            "D. Stockholm"
+        ],
+        answer: 2,
+    },
+    {
+        question: "3. What is the capital of Italy?",
+        choices: [
+            "A. Brussels",
+            "B. Rome",
+            "C. New Delhi",
+            "D. Stockholm",
+        ],
+        answer: 1,
+    },
+    {
+        question: "4. What is the capital of Pakistan?",
+        choices: [
+            "A. Islamabad",
+            "B. Buenos Aires",
+            "C. Paris",
+            "D. Prague",
+        ],
+        answer: 0,
+    },
+    {
+        question: "5. What is the capital of Cuba?",
+        choices: [
+            "A. Islamabad",
+            "B. Paris",
+            "C. Stockholm",
+            "D. Havana",
+        ],
+        answer: 3,
+    },
+    {
+        question: "6. What is the capital of United Kingdom?",
+        choices: [
+            "A. Tokyo",
+            "B. London",
+            "C. Canberra",
+            "D. Jakarta",
+        ],
+        answer: 1,
+    },
+];
