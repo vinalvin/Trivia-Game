3aa382ad5e5b8cd50e64ab526da042f14f9c77ff
Junming Zhang <vinalvin1010@gmail.com>
2019-12-05 16:25:03 -0800

    js finished and a bit from html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -78,3 +78,95 @@ function decreaseCounter() {
         showResults();
     }
 }
+
+function render() {
+
+    //create a trivial game form with 4 multiple choices
+    for (var i = 0; i < questions.length; i++) {
+        var divTag = $('<div>').addClass("form-group")
+
+        var question = questions[i]
+        console.log(question.question)
+        divTag.append($('<p>').text(question.question))
+
+        for (var x = 0; x < question.choices.length; x++) {
+            var choice = question.choices[x]
+            var div = $('<div>').attr("class", "form-check")
+            var radio = $('<input>')
+                .attr("type", "radio")
+                .attr("class", "form-check-input")
+                .attr("name", "question" + i)
+                .attr("id", "question" + i + x)
+                .attr("value", x)
+            var label = $('<label>').text(choice)
+                .attr("class", "form-check-label")
+                .attr("for", "question" + i + x)
+
+            div.append(radio, label)
+            divTag.append(div)
+        }
+        $('form').append(divTag)
+    }
+
+    //create a button to submit
+    var btn = $('<button>').text("Done")
+    $('form').append(btn)
+}
+
+function checkAnswer() {
+    /*
+        var q1 = $("input[name='question0']:checked").val()
+        var q2 = $("input[name='question1']:checked").val()
+             ^                          ^
+    */
+
+    var totalCorrectAnswer = 0;
+    for (var q = 0; q < questions.length; q++) {
+        //$("input[name='question"+q+"']:checked").val()
+        //$(`input[name='question ${q}']:checked`).val()
+
+        //convert string into a number
+        var result = parseInt($(`input[name='question${q}']:checked`).val())
+
+        //if player's answer match with the answers in the question lists, correct scrore will be increase
+        if (result === questions[q].answer) {
+            totalCorrectAnswer++
+        }
+    }
+    return totalCorrectAnswer;
+}
+
+//result of correct answer and incorrect answer
+function showResults() {
+    var correctAns = checkAnswer()
+
+    //hide form
+    $('form').hide()
+
+    //then display only results
+    $('.results').show()
+
+    //Total correct answer 
+    $('.correctAnswer').text(correctAns)
+
+    //Total incorrect answer
+    $('.incorrectAnswer').text(questions.length - correctAns)
+    console.log(correctAns)
+    console.log(questions.length - correctAns)
+
+    //display text when time runs out
+    $('.timeGoDown').text('All Done!')
+
+    //clear timer
+    clearInterval(timer)
+}
+
+//when click on the button, it will show total correct answer and incorrect answer
+$(document).on('click', 'button', function() {
+
+    //Prevent the page from refreshing
+    event.preventDefault();
+    showResults();
+});
+
+render()
